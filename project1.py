2e5406eeed029d8bb098891aa8db39cdf617f9d8
Arthur Lockman <hello@rthr.me>
2016-11-20 19:57:08 -0500

    Added saving and loading data files.

--- project1.py
+++ project1.py
@@ -34,9 +34,9 @@ def get_menu_choice(choice):
     if choice == '1':
         run_with_input_strings()
     elif choice == '2':
-        print('You chose 2')
+        generate_and_save_data()
     elif choice == '3':
-        print('You chose 3')
+        run_test_from_file()
     elif choice == '9':
         print('Goodbye!')
         exit()
@@ -54,9 +54,79 @@ def run_with_input_strings():
     string1 = input(' >>  ')
     print('Input the second string')
     string2 = input(' >>  ')
-    print(is_anagram_better_method(string1, string2))
-    print(is_anagram_brute_force(string1, string2))
-    # TODO: pretty-print output
+    pretty_print_output(string1, string2, is_anagram_better_method(string1, string2), 2)
+    pretty_print_output(string1, string2, is_anagram_brute_force(string1, string2), 1)
+
+
+def generate_and_save_data():
+    print('This selection will generate and save testing data to a file.')
+    print('Enter the filename you wish to save testing data to')
+    filename = input(' >>  ')
+    print('Enter the maximum length of string to generate in the testing data')
+    length = int(input(' >>  '))
+    print('Enter the number of entries to generate with each length')
+    entries = int(input(' >>  '))
+    print('Enter 1 if you would like to generate tests that are anagrams, '
+          '2 if you would like tests that are not anagrams, or 3 if you would like both')
+    test_type = int(input(' >>  '))
+    data = ''
+    if test_type == 1:
+        data += generate_true_test_data(length, entries)
+    elif test_type == 2:
+        data += generate_false_test_data(length, entries)
+    elif test_type == 3:
+        data += generate_true_test_data(length, entries)
+        data += generate_false_test_data(length, entries)
+    with open(filename, 'w') as f:
+        f.write(data)
+    print('Success! File', filename, 'written with testing data.')
+
+
+def run_test_from_file():
+    print('This selection will read a testing data file, and run both algorithms on it.')
+    print('Enter the filename of the testing file')
+    filename = input(' >>  ')
+    lines = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    print('File loaded.')
+    print('Enter the name of the test results file (CSV)')
+    out_filename = input(' >>  ')
+    with open(out_filename, 'w') as f:
+        f.write('String Length,String 1,String 2,Brute Force Result,Brute Force Time,Better Result,Better Time\n')
+        for line in lines:
+            output_line = ''
+            line_split = line.replace('\n', '').split(',')
+            brute_res = is_anagram_brute_force(line_split[0], line_split[1])
+            better_res = is_anagram_better_method(line_split[0], line_split[1])
+            output_line += str(len(line_split[0])) + ',' + line_split[0] + ',' + line_split[1] + ',' + str(
+                brute_res[0]) + ',' + str(brute_res[1]) + ',' + str(better_res[0]) + ',' + str(better_res[1]) + '\n'
+            pretty_print_output(line_split[0], line_split[1], brute_res, 1)
+            pretty_print_output(line_split[0], line_split[1], better_res, 2)
+            f.write(output_line)
+
+
+def pretty_print_output(string1, string2, alg_output, alg_type):
+    """
+    Prints the output from either of the anagram algorithms.
+    :param string1: The first string
+    :param string2: The second string
+    :param alg_output: output from the algorithm
+    :param alg_type: Which type, 1 is brute force, 2 is better
+    :return: nothing
+    """
+    out_string = ''
+    if alg_type == 1:
+        out_string += 'The brute force algorithm determined that '
+    if alg_type == 2:
+        out_string += 'The better algorithm determined that '
+    out_string += 'the string ' + string2 + ' '
+    if alg_output[0]:
+        out_string += 'is '
+    else:
+        out_string += 'is not '
+    out_string += 'an anagram of ' + string1 + ' in ' + str(alg_output[1]) + ' seconds.'
+    print(out_string)
 
 
 def is_anagram_brute_force(string1, string2):
@@ -69,11 +139,9 @@ def is_anagram_brute_force(string1, string2):
     second string is an anagram of the first. The second value is a float
     of the runtime of the operation.
     """
-    # TODO: DO COMPUTATION
     is_anagram = False
     start_time = time.time()
-    string1_permutations = [''.join(p) for p in permutations(string1)]
-    string1_permutations = set(string1_permutations)  # Removes duplicates
+    string1_permutations = set([''.join(p) for p in permutations(string1)])
     if string2 in string1_permutations:
         is_anagram = True
     end_time = time.time()
@@ -90,10 +158,20 @@ def is_anagram_better_method(string1, string2):
     second string is an anagram of the first. The second value is a float
     of the runtime of the operation.
     """
-    string1_letter_count = build_letter_count_dict(global_dict)
-    string2_letter_count = build_letter_count_dict(global_dict)
-    is_anagram = True
     start_time = time.time()
+    string1_letter_count = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0,
+                            'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
+                            'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0,
+                            'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
+                            'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0,
+                            'z': 0}
+    string2_letter_count = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0,
+                            'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
+                            'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0,
+                            'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
+                            'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0,
+                            'z': 0}
+    is_anagram = True
     for i in string1:
         string1_letter_count[i] += 1
     for i in string2:
@@ -104,18 +182,6 @@ def is_anagram_better_method(string1, string2):
             break
     end_time = time.time()
     return is_anagram, (end_time - start_time)
-
-
-def build_letter_count_dict(letters):
-    """
-    Build a dictionary to count letter occurrences.
-    :param letters: The letters to build the dictionary on.
-    :return: The filled dict.
-    """
-    letter_count = {}
-    for i in letters:
-        letter_count[i] = 0
-    return letter_count
 
 
 def generate_true_test_data(length, number):
@@ -129,17 +195,17 @@ def generate_true_test_data(length, number):
     :return: A string containing all of the test string data.
     """
     return_string = []
-    for i in range(1, length+1):
-        for j in range(1, number+1):
+    for i in range(1, length + 1):
+        for j in range(1, number + 1):
             initial_string = ""
-            for k in range(1, i+1):
+            for k in range(1, i + 1):
                 initial_string += random.choice(global_dict)
             string_list = list(initial_string)
             random.shuffle(string_list)
             jumbled_string = ''.join(string_list)
             anagram_string = initial_string + ',' + jumbled_string
             return_string.append(anagram_string)
-    return '\n'.join(return_string)
+    return '\n'.join(return_string) + '\n'
 
 
 def generate_false_test_data(length, number):
@@ -153,17 +219,26 @@ def generate_false_test_data(length, number):
     :return: A string containing all of the test string data.
     """
     return_string = []
-    for i in range(1, length+1):
-        for j in range(1, number+1):
+    for i in range(1, length + 1):
+        for j in range(1, number + 1):
             initial_string = ''
-            for k in range(1, i+1):
+            for k in range(1, i + 1):
                 initial_string += random.choice(global_dict)
             jumbled_string = ''
-            for k in range(1, i+1):
+            for k in range(1, i + 1):
                 jumbled_string += random.choice(global_dict)
             anagram_string = initial_string + ',' + jumbled_string
             return_string.append(anagram_string)
-    return '\n'.join(return_string)
+    return '\n'.join(return_string) + '\n'
+
+
+def effAnaBtr(s1, s2):
+    pretty_print_output(s1, s1, is_anagram_better_method(s1, s1), 2)
+
+
+def effAnaBF(s1, s2):
+    pretty_print_output(s1, s1, is_anagram_brute_force(s1, s1), 1)
+
 
 if __name__ == "__main__":
     main()
